8d7d48ecaefb630351f5bb5c8153316ae632a75c
Jo Egner <vechema@gmail.com>
2020-02-20 20:53:28 -0600

    Add more stuff for the categories. There's gotta be a better way

--- server/src/main/java/com/jegner/transaction/model/Category.java
+++ server/src/main/java/com/jegner/transaction/model/Category.java
@@ -5,18 +5,26 @@ import java.util.Arrays;
 public enum Category {
 	// expenses
 	MORTGAGE(new String[] { "direct debit jpmorgan chasechase ach" }),
-	BILLS(new String[] { "city of austin", "gas texas", "avail rent", "att*bill" }),
-	GROCERY(new String[] { "supermarkets", "groceries", "h-e-b #", "central market", "mt supermarket" }),
+	BILLS(new String[] { "city of austin", "gas texas", "avail rent", "att*bill", "grande communi" }),
+	GROCERY(
+			new String[] { "supermarkets", "groceries", "h-e-b #", "central market", "mt supermarket", "farmers mark",
+					"99 ranch", "bazar" }),
 	RESTAURANT(
-			new String[] { "restaurants", "ice crea", "phils ice", "taco", "ramen", "saam thai", "threadgills",
+			new String[] { "restaurants", "ice crea", "phils ice", "taco", "ramen", " thai", "threadgills",
 					"happy chicks", "noodle", "fruitealicious", "eurest arm", "wendy's", "pizza", "whataburger",
-					"barbecue" }),
-	TRANSPORTATION(new String[] { "gasoline", ",gas,", "h-e-b gas", "fuel", "2222 market", "shell oil", "chevron" }),
-	HEALTH(new String[] { "pharmacy" }),
-	HOBBY(new String[] { " sewing", "joann" }),
-	HOUSEHOLD(new String[] { "home depot", "new woolie", "wal-mart", "restarant sup" }),
-	PERSONAL(new String[] { "etronics", "swappa", "forlessfones" }),
-	ENTERTAINMENT(new String[] { "valve corp", "hideout theatre", "nintendo", "steam games", "novel escape", }),
+					"barbecue", "grill", "wok'n", "star of india", "be more pacific", "sq *", "dirty martins", "fooda",
+					"poke-poke", "burger", "snowy willage", "waffle", "oyster", "hoovers", "hunan lion",
+					"snowy village", "sushi", "chuy's", "arbys", "trudys", "tous les jours", "serranos", "eatery",
+					"bistro", "cheddar" }),
+	TRANSPORTATION(
+			new String[] { "gasoline", ",gas,", "h-e-b gas", "fuel", "2222 market", "shell oil", "chevron", "u-haul",
+					"parking", "wheels", "vehreg", "automotive", "oreilly" }),
+	HEALTH(new String[] { "pharmacy", "bodyspec" }),
+	HOBBY(new String[] { " sewing", "joann", "fry's" }),
+	HOUSE(new String[] { "plumbing", "home warran", "housecheck", "service wizard" }),
+	HOUSEHOLD(new String[] { "home depot", "new woolie", "wal-mart", "restaurant sup", "ikea", "wm supercent" }),
+	PERSONAL(new String[] { "etronics", "swappa", "forlessfone" }),
+	ENTERTAINMENT(new String[] { "valve corp", "the hideout", "nintendo", "steam games", "novel escape", "sherwood" }),
 	VACATION(new String[] { "emirates" }),
 	GIFT(new String[] {}),
 	PET(new String[] { "petco" }),
@@ -30,7 +38,7 @@ public enum Category {
 	// other
 	TRANSFER(new String[] { "transfer", "fid bkg svc llc", "general credit card deposit", "general withdrawal" }),
 	PAYMENT(new String[] { "automatic payment", "autopay", "pay full balance", "e-payment" }),
-	CASHBACK(new String[] { "redemption credit", "cashback" }),
+	CASHBACK(new String[] { "redemption credit", "cashback", "cash rewards" }),
 	MISCELLANEOUS(new String[] {});
 
 	private String[] descriptions;
